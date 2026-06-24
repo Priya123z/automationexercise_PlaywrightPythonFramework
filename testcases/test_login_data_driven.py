4ec3cd2a2b9751420a703a699994aed7ce2839d9
Priya <prbhagoriya@gmail.com>
2026-06-24 16:16:47 +0530

    Changing the file path in test_login_data_driven_file

--- testcases/test_login_data_driven.py
+++ testcases/test_login_data_driven.py
@@ -1,13 +1,18 @@
+from pathlib import Path
+
 import pytest
 from playwright.sync_api import expect
 
 from pages.home_page import HomePage
 from pages.login_page import LoginPage
 from utilities import data_reader_util
+from utilities.data_reader_util import read_csv_data
 
-csv_data = data_reader_util.read_csv_data("C://Users//pbhagoriya//PycharmProjects//FrameworkDevelopment//testdata//logindata.csv")
-@pytest.mark.parametrize("login_data",csv_data)
-def test_login_data_driven_testing(page,login_data):
+file_path = Path(__file__).parent.parent / "testdata" / "logindata.csv"
+csv_data = (read_csv_data(file_path))
+
+@ pytest.mark.parametrize("login_data", csv_data)
+def test_login_data_driven_testing(page, login_data):
     #creating Home Page object to navigate to the login screen via Home Page
     HomePageobj = HomePage(page)
     #Creating Login Page object to perform login operation
@@ -24,4 +29,3 @@ def test_login_data_driven_testing(page,login_data):
         expect(login_pageobj.getLoginId()).to_contain_text(" Logged in as ")
     else:
         expect(login_pageobj.login_error()).to_be_visible(timeout=3000)
-
